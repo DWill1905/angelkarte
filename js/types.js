e33a6df948cce2d0429f5a61f236803ce4ad529a
DWill1905 <David@davidwill.de>
2026-07-09 21:58:59 +0200

    Cleanup: js/types.js entfernt (nur Typen, wird zur Laufzeit nie importiert)

--- js/types.js
+++
@@ -1,4 +0,0 @@
-/* Zentrale Typdefinitionen der Angelkarte.
-   Diese Interfaces sind der eigentliche Gewinn des TypeScript-Umbaus:
-   Datenfehler in den Regionsdaten fallen beim Kompilieren auf, nicht am Wasser. */
-export {};
